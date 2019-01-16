79cabd8e727b67e83473e9ef16a3b252afdff5f1
林宥丞 <a26913700@gmail.com>
2019-01-16 14:09:37 +0800

    model連接資料表&欄位

--- app/Rent.php
+++ app/Rent.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rent extends Model
 {
-    //
+    //設定Model到對應的資料表
+    protected $table = 'rents';
+
+    protected $fillable = [
+        'site_id',
+        'user_class',
+        'user_name',
+        'user_phone',
+        'req_date',
+        'req_starttime',
+        'req_endtime',
+        'req_rentreason',
+    ];
 }
